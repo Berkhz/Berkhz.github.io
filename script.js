21840765bd4c7a22948d97e4885de2a2a37d7031
Kauan Bertalha <kauan.hb2004@gmail.com>
2023-06-04 20:57:50 -0300

    Adicionando novas validações

--- script.js
+++ script.js
@@ -1,25 +1,57 @@
-const form = document.querySelector('form');
-const cpfInput = document.querySelector('#number');
-const passwordInput = document.querySelector('#password');
+let mensagem = document.getElementsByClassName('form-control');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault(); // previne o envio do formulário
+function valorBotao() {
+  const valorCPF = document.getElementById('number').value; // Valor do CPF inserido
+  const valorEmail = document.getElementById("email").value; // Valor do email inserido
+  const valorSenha = document.getElementById("password").value; // Valor da senha inserida
 
-  const cpf = cpfInput.value;
-  const password = passwordInput.value;
+  localStorage.setItem("cpf", valorCPF);
+  localStorage.setItem("email", valorEmail);
+  localStorage.setItem("senha", valorSenha);
 
-  if (!cpf || cpf.length !== 11) {
-    alert('Por favor, insira um CPF válido.');
-    return;
+  window.location.href = "home.html";
+}
+
+//validarCPF(valorCPF);
+
+function validarCPF(valorCPF) {
+  if (valorCPF.length < 11 || valorCPF.length > 11) // CPF inválido por tamanho
+  { 
+    mensagem.innerHTML = 'Tamanho do CPF inválido!';
+  } 
+  else if (valorCPF === valorCPF.split('').reverse().join('')) // CPF inválido por caracteres iguais
+  { 
+    mensagem.innerHTML = 'CPF não pode conter todos os números iguais!';
+  } 
+  else // Validação do CPF por cálculo
+  {
+    let soma = 0;
+    for (let i = 0; i < 9; i++) {
+      soma += parseInt(valorCPF.charAt(i)) * (10 - i);
+    }
+
+    let primeiroDigito = 11 - (soma % 11);
+    if (primeiroDigito > 9) 
+    {
+      primeiroDigito = 0;
+    }
+
+    soma = 0;
+    for (let i = 0; i < 10; i++) {
+      soma += parseInt(valorCPF.charAt(i)) * (11 - i);
+    }
+    let segundoDigito = 11 - (soma % 11);
+    if (segundoDigito > 9) {
+      segundoDigito = 0;
+    }
+
+    if (parseInt(valorCPF.charAt(9)) === primeiroDigito && parseInt(valorCPF.charAt(10)) === segundoDigito) 
+    {
+      mensagem.innerHTML = 'CPF válido';
+    } 
+    else 
+    {
+      mensagem.innerHTML = 'CPF inválido';
+    }
   }
-
-  if (!password || password.length < 6) {
-    alert('Por favor, insira uma senha com pelo menos 6 caracteres.');
-    return;
-  }
-
-  // Se chegarmos até aqui, as entradas do usuário estão corretas
-  alert('Entrada válida!');
-
-  // Aqui você pode adicionar o código para enviar as informações para o servidor
-});
+}
